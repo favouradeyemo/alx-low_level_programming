fb69212ef6cc283b29cf63f8912f1503312dc415
favouradeyemo <favouradeyemo7@gmail.com>
2022-04-12 08:59:03 -0700

    My commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,21 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-*main - Entry poin
+*main - Entry point
 *
 *Return: Always 0 (Success)
 */
 int main(void)
 {
-int d;
+int d, p;
 
-for (d = '0'; d <= '9'; d++)
+for (d = '0'; d < '9'; d++)
+{
+for (p = d + 1; p <= '9'; p++)
+{
+if (p != d)
 {
 putchar(d);
-if (d != '9')
-{
+putchar(p);
+if (d == '8' && p == '9')
+continue;
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
